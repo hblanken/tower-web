d7f6de044ce970706a6b48562a8ee831759f3d09
hblanken <hanno_blankenstein@yahoo.com>
2016-05-16 23:13:15 +1000

    Remove duplicated directory

--- tower/templates/font/extract.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gst-launch-1.0 filesrc location=/var/www/html/images/solo.mkv ! decodebin ! timeoverlay ! clockoverlay halignment=right time-format="%Y/%m/%d %H:%M:%S" ! queue ! videorate ! video/x-raw,framerate=1/60 ! autovideoconvert ! jpegenc ! multifilesink location="/var/www/html/images/%04d-snap.jpg"
