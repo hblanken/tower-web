8784efed0a95a6daedd1d6c7803af2d8140edd4c
hblanken <hanno_blankenstein@yahoo.com>
2016-05-27 12:49:14 +1000

    Add files via upload

--- tower/__main__.py
+++ tower/__main__.py
@@ -127,8 +127,8 @@ def arm_and_takeoff():
             print "Basic pre-arm checks"
             # Don't try to arm until autopilot is ready
             while not vehicle.is_armable:
-            print " Waiting for vehicle to initialise..."
-            time.sleep(1)
+                print " Waiting for vehicle to initialise..."
+                time.sleep(1)
 
             print "Arming motors"
             # Copter should arm in GUIDED mode
@@ -137,8 +137,8 @@ def arm_and_takeoff():
 
             # Confirm vehicle armed before attempting to take off
             while not vehicle.armed:
-            print " Waiting for arming..."
-            time.sleep(1)
+                print " Waiting for arming..."
+                time.sleep(1)
 
             print "Taking off!"
             vehicle.simple_takeoff(request.json['alt'].upper())) # Take off to target altitude
@@ -151,7 +151,7 @@ def arm_and_takeoff():
             #        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
             #            print "Reached target altitude"
             #            break
-            time.sleep(1)
+            #time.sleep(1)
 
         except Exception as e:
             print(e)
