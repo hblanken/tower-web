0c0fe8ff3632ce8e9ad5df89667cf192e8d57cc6
hblanken <hanno_blankenstein@yahoo.com>
2016-05-27 12:38:02 +1000

    Add files via upload

--- tower/__main__.py
+++ tower/__main__.py
@@ -122,42 +122,42 @@ def api_mode():
  # Hanno added       
 @app.route("/api/takeoff", methods=['POST', 'PUT'])
 def arm_and_takeoff():
-if request.method == 'POST' or request.method == 'PUT':
+    if request.method == 'POST' or request.method == 'PUT':
         try:
     """
     Arms vehicle and fly to aTargetAltitude.
     """
 
-    print "Basic pre-arm checks"
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print " Waiting for vehicle to initialise..."
-        time.sleep(1)
+            print "Basic pre-arm checks"
+            # Don't try to arm until autopilot is ready
+            while not vehicle.is_armable:
+            print " Waiting for vehicle to initialise..."
+            time.sleep(1)
 
-    print "Arming motors"
-    # Copter should arm in GUIDED mode
-    vehicle.mode    = VehicleMode("GUIDED")
-    vehicle.armed   = True
+            print "Arming motors"
+            # Copter should arm in GUIDED mode
+            vehicle.mode    = VehicleMode("GUIDED")
+            vehicle.armed   = True
 
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print " Waiting for arming..."
-        time.sleep(1)
+            # Confirm vehicle armed before attempting to take off
+            while not vehicle.armed:
+            print " Waiting for arming..."
+            time.sleep(1)
 
-    print "Taking off!"
-    vehicle.simple_takeoff(request.json['alt'].upper())) # Take off to target altitude
+            print "Taking off!"
+            vehicle.simple_takeoff(request.json['alt'].upper())) # Take off to target altitude
 
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
-    #while True:
-#        print " Altitude: ", vehicle.location.global_relative_frame.alt
-        #Break and return from function just below target altitude.
-#        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
-#            print "Reached target altitude"
-#            break
-        time.sleep(1)
+            # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+            #  after Vehicle.simple_takeoff will execute immediately).
+            #while True:
+            #        print " Altitude: ", vehicle.location.global_relative_frame.alt
+            #Break and return from function just below target altitude.
+            #        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            #            print "Reached target altitude"
+            #            break
+            time.sleep(1)
 
-    except Exception as e:
+        except Exception as e:
             print(e)
             return jsonify(ok=False)
 # End add Hanno
