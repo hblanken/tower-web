63b3b881fab34332fae948659754fca600c55966
hblanken <hanno_blankenstein@yahoo.com>
2016-06-03 13:55:53 +1000

    Add files via upload

--- tower/__main__.py
+++ tower/__main__.py
@@ -175,6 +175,166 @@ def arm_and_takeoff():
         except Exception as e:
             print(e)
             return jsonify(ok=False)
+        
+def condition_yaw(heading, relative=False):
+##    """
+##    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
+##
+##    This method sets an absolute heading by default, but you can set the `relative` parameter
+##    to `True` to set yaw relative to the current yaw heading.
+##
+##    By default the yaw of the vehicle will follow the direction of travel. After setting 
+##    the yaw using this function there is no way to return to the default yaw "follow direction 
+##    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
+##
+##    For more information see: 
+##    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
+##    """
+    if relative:
+        is_relative = 1 #yaw relative to direction of travel
+    else:
+        is_relative = 0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+
+
+        # send command to vehicle on 1 Hz cycle
+    for x in range(0,duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+
+#Set up velocity vector to map to each direction.
+# vx > 0 => fly North
+# vx < 0 => fly South
+FORWARD = 2
+BACK = -2
+
+# Note for vy:
+# vy > 0 => fly East
+# vy < 0 => fly West
+RIGHT = 2
+LEFT = -2
+
+# Note for vz: 
+# vz < 0 => ascend
+# vz > 0 => descend
+UP = -0.5
+DOWN = 0.5
+
+DURATION = 10
+                    
+@app.route("/api/left", methods=['POST', 'PUT'])
+def left():
+    if request.method == 'POST' or request.method == 'PUT':    
+        try:
+            print("Set groundspeed to 2m/s.")
+            vehicle.groundspeed=2
+            print("Moving left for 10 sec.")
+            send_ned_velocity(0,LEFT,0,DURATION)
+            vehicle.flush()
+            return jsonify(ok=True)
+        except Exception as e:
+            print(e)
+            return jsonify(ok=False)
+@app.route("/api/right", methods=['POST', 'PUT'])
+def right():
+    if request.method == 'POST' or request.method == 'PUT':    
+        try:
+            print("Set groundspeed to 2m/s.")
+            vehicle.groundspeed=2
+            print("Moving right for 10 sec.")
+            send_ned_velocity(0,RIGHT,0,DURATION)
+            vehicle.flush()
+            return jsonify(ok=True)
+        except Exception as e:
+            print(e)
+            return jsonify(ok=False)
+@app.route("/api/forward", methods=['POST', 'PUT'])
+def forward():
+    if request.method == 'POST' or request.method == 'PUT':    
+        try:
+            print("Set groundspeed to 2m/s.")
+            vehicle.groundspeed=2
+            print("Moving forward for 10 sec.")
+            send_ned_velocity(FORWARD,0,0,DURATION)
+            vehicle.flush()
+            return jsonify(ok=True)
+        except Exception as e:
+            print(e)
+            return jsonify(ok=False)
+@app.route("/api/back", methods=['POST', 'PUT'])
+def back():
+    if request.method == 'POST' or request.method == 'PUT':    
+        try:
+            print("Set groundspeed to 2m/s.")
+            vehicle.groundspeed=2
+            print("Moving back for 10 sec.")
+            send_ned_velocity(BACK,0,0,DURATION)
+            vehicle.flush()
+            return jsonify(ok=True)
+        except Exception as e:
+            print(e)
+            return jsonify(ok=False)
+@app.route("/api/up", methods=['POST', 'PUT'])
+def up():
+    if request.method == 'POST' or request.method == 'PUT':    
+        try:
+            print("Set groundspeed to 0.5m/s.")
+            vehicle.groundspeed=0.5
+            print("Moving up for 1 sec.")
+            send_ned_velocity(0,0,UP,1)
+            vehicle.flush()
+            return jsonify(ok=True)
+        except Exception as e:
+            print(e)
+            return jsonify(ok=False)
+@app.route("/api/down", methods=['POST', 'PUT'])
+def down():
+    if request.method == 'POST' or request.method == 'PUT':    
+        try:
+            print("Set groundspeed to 0.5m/s.")
+            vehicle.groundspeed=0.5
+            print("Moving down for 1 sec.")
+            send_ned_velocity(0,0,DOWN,1)
+            vehicle.flush()
+            return jsonify(ok=True)
+        except Exception as e:
+            print(e)
+            return jsonify(ok=False)
+@app.route("/api/yaw", methods=['POST', 'PUT'])
+def yaw():
+    if request.method == 'POST' or request.method == 'PUT':    
+        try:
+            print("Yaw 5 relative (to previous yaw heading)")
+            condition_yaw(5,relative=True)
+            vehicle.flush()
+            return jsonify(ok=True)
+        except Exception as e:
+            print(e)
+            return jsonify(ok=False)
+        
+        
 # End add Hanno
 
 # Never cache
